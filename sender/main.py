13ac6db6576a3ab1a9da5f87ac0da913baafc9e5
GitHub-Laziji <c_smtp@163.com>
2020-10-03 21:14:39 +0800

    init

--- sender/main.py
+++ sender/main.py
@@ -1,11 +1,13 @@
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import os
+from resources.config import SEGMENTFAULT_USERNAME, SEGMENTFAULT_PASSWORD
 
 
+print(SEGMENTFAULT_USERNAME)
 options = Options()
 # options.add_argument('--headless')
-driver = webdriver.Chrome(executable_path=os.path.abspath('./chromedriver.exe'), options=options)
+driver = webdriver.Chrome(executable_path=os.path.abspath('../resources/chromedriver.exe'), options=options)
 
 driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
     "source": """
