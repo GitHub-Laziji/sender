8e61668e7178c4b07f96524e75f05b21b8d180cd
GitHub-Laziji <c_smtp@163.com>
2020-10-03 22:47:27 +0800

    init

--- sender/main.py
+++ sender/main.py
@@ -3,8 +3,6 @@ from selenium.webdriver.chrome.options import Options
 import os
 from resources.config import SEGMENTFAULT_USERNAME, SEGMENTFAULT_PASSWORD
 
-
-print(SEGMENTFAULT_USERNAME)
 options = Options()
 # options.add_argument('--headless')
 driver = webdriver.Chrome(executable_path=os.path.abspath('../resources/chromedriver.exe'), options=options)
@@ -18,3 +16,7 @@ driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument", {
 })
 
 driver.get("https://segmentfault.com/")
+driver.implicitly_wait(1)
+driver.set_window_size(1366, 768)
+driver.implicitly_wait(1)
+driver.find_element_by_css_selector(".SFRegister.btn-signin").click()
